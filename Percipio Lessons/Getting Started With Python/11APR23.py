7a930c893b778d1396aa1bbc1437117aa68633dc
Grady <43967636+Rckymtnskier@users.noreply.github.com>
2023-04-11 13:48:57 +0000

    Done Studying for the day

---
+++ Percipio Lessons/Getting Started With Python/11APR23.py
@@ -0,0 +1,37 @@
+n1 = 34
+n2 = 34.5
+n3 = 100
+s1 = 'Hello'
+s2 = 'World'
+
+total_n = n1 + n2 + n3
+print(total_n)
+
+
+n1 = n1 + 1
+print(n1)
+
+n2 = n2 + 10
+print(n2)
+
+n3 = n3 - 50
+print(n3)
+
+n1 += n1 + 1
+print(n1)
+
+n2 += 10
+print(n2)
+
+x1 = 1
+
+my_var = "barks"
+
+print(f'A dog {my_var}!')
+
+# '\t' is the TAB special character
+#If you need to use a '\' in a string you need to use 2 '\' i.e. 'C:\\home\\Desktop'
+
+#Exercise
+
+
